e4f8b847e3bb52cb480bd0cae4124e68e2199b27
Gaetano Piazzolla <gae.piaz@gmail.com>
2025-06-07 13:07:54 +0200

    Enhance ConfigController to include DB connection time and HikariCP pool size; add entrypoint script for network delay configuration

--- src/main/java/gae/piaz/boot/virtual/rest/ConfigController.java
+++ src/main/java/gae/piaz/boot/virtual/rest/ConfigController.java
@@ -4,15 +4,28 @@ import org.springframework.boot.SpringBootVersion;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @RestController
 @RequestMapping("/config")
 public class ConfigController {
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @GetMapping("/")
     public String getConfigs() {
-        return String.format("<h1>-SpringBoot Version: %s <br/>-Java: %s <br/>-Thread %s</h1>", SpringBootVersion.getVersion(),
-            System.getProperty("java.version"), Thread.currentThread());
+        long startTime = System.currentTimeMillis();
+        jdbcTemplate.queryForObject("SELECT 1", Integer.class);
+        long endTime = System.currentTimeMillis();
+        
+        return String.format("<h1>-SpringBoot Version: %s <br/>-Java: %s <br/>-Thread %s <br/>-HikariCP Pool Size: %s <br/>-DB Connection Time: %dms</h1>", 
+            SpringBootVersion.getVersion(),
+            System.getProperty("java.version"), 
+            Thread.currentThread(),
+            System.getenv("HIKARICP_CONNECTION_POOL"),
+            endTime - startTime);
     }
 
 }
